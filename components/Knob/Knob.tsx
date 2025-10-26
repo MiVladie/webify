8f7f4d27d6f1e21197f4237ccde1eceb7426e4b5
MiVladie <justvladon@outlook.com>
2025-10-26 12:04:20 +0200

    ✨ new animations

--- components/Knob/Knob.tsx
+++ components/Knob/Knob.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { clsx } from '@/lib/style';
 
 import classes from './Knob.module.scss';
@@ -11,13 +12,45 @@ interface Props {
 	className?: string;
 }
 
-const Knob = ({ icon, disabled, onClick, className }: Props) => (
-	<div
-		className={clsx(classes.Knob, { [classes.Disabled]: !!disabled }, className)}
-		onClick={() => (!disabled ? onClick?.() : null)}
-		role='button'>
-		<i className={classes.Icon}>{icon}</i>
-	</div>
-);
+const Knob = ({ icon, disabled, onClick, className }: Props) => {
+	const [clicked, setClicked] = useState<boolean>(false);
+
+	useEffect(() => {
+		let timeout: NodeJS.Timeout;
+
+		if (clicked && !disabled) {
+			timeout = setTimeout(() => {
+				setClicked(false);
+			}, 500);
+		}
+
+		return () => clearTimeout(timeout);
+	}, [clicked, disabled]);
+
+	function handleClick() {
+		if (disabled) {
+			return;
+		}
+
+		setClicked(true);
+
+		onClick?.();
+	}
+
+	return (
+		<div
+			className={clsx(classes.Knob, { [classes.Disabled]: !!disabled }, className)}
+			onClick={handleClick}
+			role='button'>
+			<i
+				className={clsx(classes.Icon, {
+					[classes.IconReady]: !clicked,
+					[classes.IconClicked]: clicked
+				})}>
+				{icon}
+			</i>
+		</div>
+	);
+};
 
 export default Knob;
